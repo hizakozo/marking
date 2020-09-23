3e97de5bc215a10e3fed8ca5f4a65c9774b82d6e
kendo yasui <kendoyasui@kendouyasuinoMacBook-Pro.local>
2020-09-23 19:26:49 +0900

    修正２1

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import time
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+
 # コメントテスト
 load_dotenv(verbose=True)
 
@@ -28,81 +29,104 @@ count = 0
 lastUserId = ''
 lastIndex = 0
 
-def elementCheck(className):
-    notIsExist = True
-    while notIsExist:
+
+def element_check_index(class_name):
+    not_is_exist = True
+    while not_is_exist:
         try:
-            if(WebDriverWait(driver, 1).until(EC.visibility_of_element_located((By.CLASS_NAME, className))).is_displayed()):
-                notIsExist = False
+            if (WebDriverWait(driver, 1).until(
+                    EC.visibility_of_element_located((By.CLASS_NAME, class_name))).is_displayed()):
+                not_is_exist = False
         except:
             print('ページリロード')
             driver.refresh()
             time.sleep(2)
             pass
-    
+
+
+def element_check_profile(class_name):
+    number_of_trials = 0
+    not_is_exist = True
+    while number_of_trials != 3 and not_is_exist:
+        try:
+            if (WebDriverWait(driver, 1).until(
+                    EC.visibility_of_element_located((By.CLASS_NAME, class_name))).is_displayed()):
+                not_is_exist = False
+        except:
+            number_of_trials += 1
+            print('プロフィールページリロード')
+            driver.refresh()
+            time.sleep(2)
+            pass
+    if number_of_trials >= 3:
+        driver.back()
+
 
 # clickして戻る
-def clickAndReturn(userId):
-    noErr = True
-    elementCheck('user-card-small_main-photo')
+def click_and_return(user_id):
+    no_err = True
+    element_check_index('user-card-small_main-photo')
     try:
-        driver.find_element_by_id(userId).click()
+        driver.find_element_by_id(user_id).click()
     except:
-        noErr = False
+        no_err = False
         print("要素なし")
         pass
-    
+
     try:
         ele = driver.find_element_by_xpath('/html/body/center/h1')
-        if(ele.text == '502 Bad Gateway'):
+        if ele.text == '502 Bad Gateway':
             print('502 Bad Gateway')
-            noErr = False
+            no_err = False
             driver.refresh()
     except:
         pass
 
-    if(noErr):
+    if no_err:
         global count
         count += 1
-        print("-- click to: " + userId + " --- count: " + str(count) +" / " + every_one)
-        elementCheck('profile-thumb_photo_img')
+        print("-- click to: " + user_id + " --- count: " + str(count) + " / " + every_one)
+        element_check_profile('profile-thumb_photo_img')
         driver.back()
 
-#1,クリック対象ユーザーの作成
-def createUserList():
+
+# 1,クリック対象ユーザーの作成
+def create_user_list():
     global lastUserId
     global lastIndex
-    userList = []
+    user_list = []
     for i, g in enumerate(driver.find_elements_by_class_name("link-area")):
-        id = g.get_attribute("id")
-        userList.append(id)
-    if (lastUserId != ''):
+        user_list.append(g.get_attribute("id"))
+    if lastUserId != '':
         try:
-            del userList[:userList.index(lastUserId) +1]
+            del user_list[:user_list.index(lastUserId) + 1]
         except:
             print("last user id is empty")
-            del userList[:lastIndex]
+            del user_list[:lastIndex]
             pass
-    print(userList)
-    lastUserId = userList[-1]
-    lastIndex = len(userList)
-    return userList
+    print(user_list)
+    lastUserId = user_list[-1]
+    lastIndex = len(user_list)
+    return user_list
 
-#2,ターゲットを全てクリック
-def marking(userList):
-    for userId in userList:
-        if(count == int(every_one[:len(every_one)-2]) + 20):
-            driver.close()
-        clickAndReturn(userId)
 
-#3,ページのロード
-def loadPage():
+# 2,ターゲットを全てクリック
+def marking(user_list):
+    for userId in user_list:
+        click_and_return(userId)
+
+
+# 3,ページのロード
+def load_page():
     time.sleep(1)
     driver.execute_script("window.scrollBy(0, -50);")
     time.sleep(2)
 
-#1,2,3を繰り返す
-while True:
-    userList = createUserList()
+
+# 1,2,3を繰り返す
+while count == int(every_one[:len(every_one) - 2]) + 20:
+    userList = create_user_list()
     marking(userList)
-    loadPage()
+    load_page()
+
+driver.close()
